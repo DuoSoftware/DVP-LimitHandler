e9e9b0a52d7311499f82bf2610f166df62be0b97
Pawan Sasanka <pawan@duosoftware.com>
2015-06-04 09:29:52 +0530

    module uninstalled

--- SheduleApi.js
+++ SheduleApi.js
@@ -6,7 +6,6 @@
 
 var DbConn = require('DVP-DBModels');
 var restify = require('restify');
-var sre = require('swagger-restify-express');
 /*var context=require('./SIPUserEndpointService.js');
  var UACCreate=require('./CreateSipUACrec.js');
  var Extmgt=require('./ExtensionManagementAPI.js');
