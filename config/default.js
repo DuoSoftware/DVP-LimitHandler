e2f0104a2444abbbd0faefb8d89ab3af4dcd56d1
sine19 <sineth17@gmail.com>
2018-06-05 14:27:38 +0530

    clear all values
    remove all default.js and conf.js values

--- config/default.js
+++ config/default.js
@@ -1,21 +1,21 @@
 module.exports = {
   "DB": {
     "Type":"postgres",
-    "User":"duo",
-    "Password":"DuoS123",
+    "User":"",
+    "Password":"",
     "Port":5432,
-    "Host":"104.236.231.11",
-    "Database":"duo"
+    "Host":"",
+    "Database":""
   },
   "Redis":
   {
     "mode":"sentinel",//instance, cluster, sentinel
-    "ip": "45.55.142.207",
+    "ip": "",
     "port": 6389,
-    "user": "duo",
-    "password": "DuoS123",
+    "user": "",
+    "password": "",
     "sentinels":{
-      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "hosts": "",
       "port":16389,
       "name":"redis-cluster"
     }
@@ -26,13 +26,13 @@ module.exports = {
   "Security":
   {
 
-    "ip" : "45.55.142.207",
+    "ip" : "",
     "port": 6389,
-    "user": "duo",
-    "password": "DuoS123",
+    "user": "",
+    "password": "",
     "mode":"sentinel",//instance, cluster, sentinel
     "sentinels":{
-      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "hosts": "",
       "port":16389,
       "name":"redis-cluster"
     }
@@ -50,19 +50,19 @@ module.exports = {
   {
     "NotificationService":
     {
-      "domain":"notificationservice.app.veery.cloud",
+      "domain":"",
       "version":"1.0.0.0"
     },
     "UserService":
     {
-      "domain":"userservice.app.veery.cloud",
+      "domain":"",
       "version":"1.0.0.0"
     },
     "AppRegistry":
     {
-      "domain":"appregistry.app.veery.cloud",
+      "domain":"",
       "version":"1.0.0.0"
     }
   },
-  "Token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkaW51c2hhZGNrIiwianRpIjoiMzc3YmU1NGUtMzZjNi00MzM0LTk5YjMtZjAxOTdlZThlMWZjIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDUwNjYyNDgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzMwNjYyNDh9.AhVLiLtNdUjcY9fLhIlaR1G7Wh00eSr6z7GD42z8jQ8"
+  "Token":""
 };
